3909bebed3e2a1f6fc18904300cd81682ae816fc
katfish125 <kathryn125yu@gmail.com>
2017-09-04 01:56:59 -0700

    added all project files for ch1

--- ch1/src/StringRotation.java
+++ ch1/src/StringRotation.java
@@ -1,5 +1,14 @@
 public class StringRotation {
     public static void main(String[] args) {
+        // had to take a hint for this one :(
+        String str_orig = "b4rotate";
+        String str_aft = "4rotateb";
 
+        if ((str_orig+str_orig).contains(str_aft)) {
+            System.out.println("There was a rotation.");
+        }
+        else {
+            System.out.println("No rotation.");
+        }
     }
 }
