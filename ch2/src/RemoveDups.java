d8228d6eb991604402de6f0042b6980553661c81
Kathryn Yu <kathryn.yu@mail.utoronto.ca>
2017-09-05 16:47:47 -0700

    Add files via upload

---
+++ ch2/src/RemoveDups.java
@@ -0,0 +1,12 @@
+public class RemoveDups {
+    public static void main(String[] args) {
+        LinkedList test = new LinkedList(0);
+        test.append(1);
+        test.append(2);
+        test.append(2);
+        test.append(3);
+        test.append(3);
+        test.append(3);
+        test.append(4);
+    }
+}
